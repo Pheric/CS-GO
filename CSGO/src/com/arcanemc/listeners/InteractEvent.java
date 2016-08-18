1d57d8c8e543caf94438b672bb692362cb849da6
photon156 <>
2016-08-18 12:59:17 -0500

    Working. Sprint, walk, sneak update

--- CSGO/src/com/arcanemc/listeners/InteractEvent.java
+++ CSGO/src/com/arcanemc/listeners/InteractEvent.java
@@ -1,39 +1,112 @@
 package com.arcanemc.listeners;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.event.player.PlayerToggleSprintEvent;
 import org.bukkit.util.Vector;
 
 import com.arcanemc.main.Main;
 
-public class InteractEvent implements Listener{
+public class InteractEvent implements Listener{ //Min+(int)(Math.random()*((Max-Min)+1))
 	private final Main Main;
 	public InteractEvent(Main Main){
 		this.Main=Main;
 		Bukkit.getServer().getPluginManager().registerEvents(this,Main);
+		guns=(Set<String>)Main.getConfig().getConfigurationSection("guns").getKeys(true);
+		sneaking=new HashSet<Player>();
+		running=new HashSet<Player>();
 	}
 	Player target;
-	double rX,rY,rZ;
+	Set<String> guns;
+	Set<Player> sneaking,running;
 	@EventHandler
 	public void RC(PlayerInteractEvent event){
 		if(event.getPlayer()!=null){
 			target=event.getPlayer();
-			if(event.getMaterial()!=null&&event.getMaterial()==Material.STONE_HOE){	
-				if(event.getAction()==Action.RIGHT_CLICK_AIR||event.getAction()==Action.RIGHT_CLICK_BLOCK){ //The player shoots
-					//Min+(int)(Math.random()*((Max-Min)+1))
-					rX=Main.min+(Math.random()*((Main.max-Main.min)+1)); //Gets a random number between the min and max that you put in the command
-					rY=Main.min+(Math.random()*((Main.max-Main.min)+1)); // "
-					rZ=Main.min+(Math.random()*((Main.max-Main.min)+1)); // "
-					Snowball bullet=target.getWorld().spawn(target.getEyeLocation(),Snowball.class); // Put a snowball in the player's head
-					bullet.setVelocity(target.getEyeLocation().getDirection().multiply(4)); // Speed up the bullet 4x
-					bullet.setVelocity(bullet.getVelocity().add(new Vector(rX-.55,rY-.3,rZ))); // Modify the direction by adding the random numbers. Also, my attempted fix for the bug
-					bullet.setShooter(target); //Makes sure the snowball passes through the person that shot it
+			if(event.getAction()==Action.RIGHT_CLICK_AIR||event.getAction()==Action.RIGHT_CLICK_BLOCK&&event.getItem()!=null){ //TODO: Ingame check?
+				if(!sneaking.contains(target)&&!running.contains(target)){
+					for(String name:guns){
+						if(event.getItem().getItemMeta().getDisplayName().contains(name)){
+							double rX=Math.random()/Main.getConfig().getDouble("guns."+name+".accuracyDbl"); //Gets a random number between the min and max that you put in the command
+							double rY=Math.random()/Main.getConfig().getDouble("guns."+name+".accuracyDbl"); // "
+							double rZ=Math.random()/Main.getConfig().getDouble("guns."+name+".accuracyDbl"); // "
+							double cX=Main.getConfig().getDouble("guns."+name+".correctXdbl");
+							double cY=Main.getConfig().getDouble("guns."+name+".correctYdbl");
+							double cZ=Main.getConfig().getDouble("guns."+name+".correctZdbl");
+							double spd=Main.getConfig().getDouble("guns."+name+".speedDbl");
+							Snowball bullet=target.getWorld().spawn(target.getEyeLocation(),Snowball.class); // Put a snowball in the player's head
+							bullet.setVelocity((target.getEyeLocation().getDirection().multiply(spd)).add(new Vector(rX-cX,rY-cY,rZ-cZ))); // Speed up the bullet 4x
+							bullet.setShooter(target); //Makes sure the snowball passes through the person that shot it | -.55 -.3 -.45
+							break; //It can't be two guns at once...
+						}
+					}
+				}else if(sneaking.contains(target)){
+					for(String name:guns){
+						if(event.getItem().getItemMeta().getDisplayName().contains(name)){
+							double rX=Math.random()/Main.getConfig().getDouble("guns."+name+".SaccuracyDbl"); //Gets a random number between the min and max that you put in the command
+							double rY=Math.random()/Main.getConfig().getDouble("guns."+name+".SaccuracyDbl"); // "
+							double rZ=Math.random()/Main.getConfig().getDouble("guns."+name+".SaccuracyDbl"); // "
+							double cX=Main.getConfig().getDouble("guns."+name+".ScorrectXdbl");
+							double cY=Main.getConfig().getDouble("guns."+name+".ScorrectYdbl");
+							double cZ=Main.getConfig().getDouble("guns."+name+".ScorrectZdbl");
+							double spd=Main.getConfig().getDouble("guns."+name+".SspeedDbl");
+							Snowball bullet=target.getWorld().spawn(target.getEyeLocation(),Snowball.class); // Put a snowball in the player's head
+							bullet.setVelocity((target.getEyeLocation().getDirection().multiply(spd)).add(new Vector(rX-cX,rY-cY,rZ-cZ))); // Speed up the bullet 4x
+							bullet.setShooter(target); //Makes sure the snowball passes through the person that shot it | -.55 -.3 -.45
+							break; //It can't be two guns at once...
+						}
+					}
+				}else{
+					for(String name:guns){
+						if(event.getItem().getItemMeta().getDisplayName().contains(name)){
+							double rX=Math.random()/Main.getConfig().getDouble("guns."+name+".RaccuracyDbl"); //Gets a random number between the min and max that you put in the command
+							double rY=Math.random()/Main.getConfig().getDouble("guns."+name+".RaccuracyDbl"); // "
+							double rZ=Math.random()/Main.getConfig().getDouble("guns."+name+".RaccuracyDbl"); // "
+							double cX=Main.getConfig().getDouble("guns."+name+".RcorrectXdbl");
+							double cY=Main.getConfig().getDouble("guns."+name+".RcorrectYdbl");
+							double cZ=Main.getConfig().getDouble("guns."+name+".RcorrectZdbl");
+							double spd=Main.getConfig().getDouble("guns."+name+".RspeedDbl");
+							Snowball bullet=target.getWorld().spawn(target.getEyeLocation(),Snowball.class); // Put a snowball in the player's head
+							bullet.setVelocity((target.getEyeLocation().getDirection().multiply(spd)).add(new Vector(rX-cX,rY-cY,rZ-cZ))); // Speed up the bullet 4x
+							bullet.setShooter(target); //Makes sure the snowball passes through the person that shot it | -.55 -.3 -.45
+							break; //It can't be two guns at once...
+						}
+					}
+				}
+			}
+		}
+	}
+	@EventHandler
+	public void sneaking(PlayerToggleSneakEvent event){
+		if(event.getPlayer()!=null){
+			target=event.getPlayer();
+			if(event.isSneaking()){
+				sneaking.add(target);
+			}else{
+				if(sneaking.contains(target)){
+					sneaking.remove(target);
+				}
+			}
+		}
+	}
+	@EventHandler
+	public void running(PlayerToggleSprintEvent event){
+		if(event.getPlayer()!=null){
+			target=event.getPlayer();
+			if(event.isSprinting()){
+				running.add(target);
+			}else{
+				if(running.contains(target)){
+					running.remove(target);
 				}
 			}
 		}
--- CSGO/src/com/arcanemc/main/Main.java
+++ CSGO/src/com/arcanemc/main/Main.java
@@ -1,28 +1,43 @@
 package com.arcanemc.main;
 
+import java.util.Set;
+
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.arcanemc.listeners.InteractEvent;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class Main extends JavaPlugin{
 	@Override
 	public void onEnable(){
+		this.saveDefaultConfig();
 		new InteractEvent(this);
+		guns=(Set<String>)this.getConfig().getConfigurationSection("guns").getKeys(false);
 	}
 	Player target;
-	public float min,max,x,y;
+	Set<String> guns;
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
 		if (sender != null && sender instanceof Player) {
 			target = (Player) sender;
-			if(lbl.equalsIgnoreCase("setrange")){
-				min=Float.parseFloat(args[0]);
-				max=Float.parseFloat(args[1]);
-				x=Float.parseFloat(args[2]);
-				y=Float.parseFloat(args[3]);
+			if(lbl.equalsIgnoreCase("getgun")){
+				for(String name:guns){
+					if(args[0].equalsIgnoreCase(name)){
+						ItemStack gun=new ItemStack(Material.getMaterial(this.getConfig().getString("guns."+name+".material")));
+						ItemMeta meta=gun.getItemMeta();
+						meta.setDisplayName(ChatColor.RED+name);
+						gun.setItemMeta(meta);
+						target.getInventory().addItem(gun);
+						break;
+					}
+				}
 			}
 		}
 		return true;
